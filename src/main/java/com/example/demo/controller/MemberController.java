6be6a9422995799bf9f45fe9f3c73692f624a685
우재 <goodsj1003@naver.com>
2025-08-10 14:06:44 +0900

    초기 커밋

---
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,85 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.MemberRequest;
+import com.example.demo.dto.MemberResponse;
+import com.example.demo.model.Member;
+import com.example.demo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.service.MemberService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberResponse post(@RequestBody MemberRequest memberRequest) {
+        return memberService.create(memberRequest);
+    }
+
+    @GetMapping
+    public List<MemberResponse> getAll() {
+        return memberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public MemberResponse get(@PathVariable("id") Long id) {
+        return memberService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MemberResponse put(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.update(id, memberRequest);
+    }
+
+    @PatchMapping("/{id}")
+    public MemberResponse patch(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.patch(id, memberRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        memberService.deleteById(id);
+    }
+/*
+    @GetMapping
+    public List<Member> getAll() {
+        return memberRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Member get(@PathVariable("id") Long id) {
+        return memberRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Member put(@PathVariable("id") Long id, @RequestBody Member member) {
+        member.setId(id);
+        return memberRepository.save(member);
+    }
+
+    @PatchMapping("/{id}")
+    public Member patch(@PathVariable("id") Long id, @RequestBody Member patch) {
+        Member member = memberRepository.findById(id).orElse(null);
+        if(member != null) {
+            if(patch.getName() != null) member.setName(patch.getName());
+            if(patch.getEmail() != null) member.setEmail(patch.getEmail());
+            if(patch.getAge() != null) member.setAge(patch.getAge());
+            memberRepository.save(member);
+        }
+        return member;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        memberRepository.deleteById(id);
+    }
+
+ */
+}
